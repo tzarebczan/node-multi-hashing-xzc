1d354414354dc57554d0463f7eff41dca511a643
Thomas Zarebczan <tzarebczan@users.noreply.github.com>
2017-02-19 14:45:17 -0500

    Add files via upload

--- pascal.h
+++ pascal.h
@@ -1,10 +1,16 @@
 #ifndef PASCAL_H
 #define PASCAL_H
 
+extern "C" {
+#endif
 
-extern int pascal_test(unsigned char *pdata, const unsigned char *ptarget, uint32_t nonce);
-extern void pascal_prepare_work(dev_blk_ctx *blk, uint32_t *state, uint32_t *pdata);
-extern void pascal_midstate(struct work *work);
-extern void pascal_regenhash(struct work *work);
+
+int pascal_test(unsigned char *pdata, const unsigned char *ptarget, uint32_t nonce);
+void pascal_prepare_work(dev_blk_ctx *blk, uint32_t *state, uint32_t *pdata);
+void pascal_midstate(struct work *work);
+void pascal_regenhash(struct work *work);
+
+#ifdef __cplusplus
+}
 
 #endif /* PASCAL_H */
